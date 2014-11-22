0a70a9e90e83ee461839031f6817ae86346e1794
fervicente <fervicente@gmail.com>
2014-11-22 10:28:04 +0100

    Programm assignment 22NOV

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Defines a matrix object and methods to compute the inverse
+## storing the obtained results in cached memory. Only works
+## with invertible matrices (no checking is performed). PA2-Rprogramming
 
-## Write a short comment describing this function
-
+## Defines the matrix object, getters and setters
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL # init matrix inverse
+        
+        # Allocate data in matrix object
+        set <- function(y) {
+          x <<- y
+          i <<- NULL
+        }
+        
+        # Get data from matrix object
+        get <- function() x
+        
+        # Set inverse value 'i'
+        setinverse <- function(inverse) i <<- inverse
+        
+        # Get inverse value 'i'
+        getinverse <- function() i
+        
+        # List of implemented functions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes matrix inversion, if exists gets cached value
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse() # Get inverse into variable 'i'
+        if(!is.null(i)) {
+          # if the inverse was already calculated provide cached data
+          message("getting cached data")
+          return(i)
+        }
+        
+        # if the inverse was not calculated, do it now
+        data <- x$get()       # get matrix data
+        i <- solve(data, ...) # compute matrix inverse
+        x$setinverse(i)       # allocate inverse value for future queries
+        i
 }
